9f7d28b0db166f22a82245bfe1b5697b173003b7
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2014-08-20 22:29:41 +0200

    Added Json attribute PropertyNames in DtoTicker. Added A project to collect all example of usages in.

--- ViolinBTCE.Dto/DtoTicker.cs
+++ ViolinBTCE.Dto/DtoTicker.cs
@@ -1,18 +1,52 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace ViolinBtce.Dto
 {
     [Serializable]
 	public class DtoTicker
 	{
+        [JsonProperty(PropertyName = "avg")]
 		public decimal Average { get; set; }
 		public decimal Buy { get; set; }
 		public decimal High { get; set; }
 		public decimal Last { get; set; }
 		public decimal Low { get; set; }
 		public decimal Sell { get; set; }
-		public decimal Volume { get; set; }
+
+        [JsonProperty(PropertyName = "vol")]
+        public decimal Volume { get; set; }
+
+        [JsonProperty(PropertyName = "vol_cur")]
 		public decimal VolumeCurrent { get; set; }
-		public UInt32 ServerTime { get; set; }
+
+        [JsonProperty(PropertyName = "server_time")]
+        public UInt32 ServerTime { get; set; }
+
+        public UInt32 Updated { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Buy: {0}\n" +
+                                 "Sell:{1}\n" +
+                                 "Low: {2}\n" +
+                                 "Average: {3}\n" +
+                                 "High: {4}\n" +
+                                 "Last: {5}\n" +
+                                 "Volume: {6}\n" +
+                                 "Current Volume: {7}\n" +
+                                 "Server Time: {8}\n" +
+                                 "Updated: {9}",
+                                 Buy,
+                                 Sell,
+                                 Low,
+                                 Average,
+                                 High,
+                                 Last,
+                                 Volume,
+                                 VolumeCurrent,
+                                 ServerTime,
+                                 Updated);
+        }
 	}
 }
---
+++ ViolinBTCE.UsageExample/ExampleProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ViolinBtce.Dto;
+using ViolinBtce.Dto.Enums;
+
+namespace ViolinBTCE.UsageExample
+{
+    public class ExampleProgram
+    {
+        public static void Main(string[] args)
+        {
+            ViolinBtce violinBtce = new ViolinBtce("key","secret");
+
+            DtoTicker ticker = violinBtce.GetTicker(Pair.eur_usd);
+
+            Console.WriteLine(ticker.ToString());
+            Console.ReadLine();
+        }
+    }
+}
